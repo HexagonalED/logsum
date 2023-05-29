5fac7a6bd32c8d757f40afe09f2fb235300917cd
shuin3180 <shuin3180@naver.com>
2023-05-29 17:01:34 +0900

    Hard_coding (logSumCoefficient)

--- denoising_diffusion_pytorch/denoising_diffusion_pytorch.py
+++ denoising_diffusion_pytorch/denoising_diffusion_pytorch.py
@@ -27,10 +27,39 @@ from accelerate import Accelerator
 import numpy as np
 from pytorch_fid.inception import InceptionV3
 from pytorch_fid.fid_score import calculate_frechet_distance
-from denoising_diffusion_pytorch import logSumCoefficient
+#from denoising_diffusion_pytorch import logSumCoefficient
 #####
 #from denoising_diffusion_pytorch.version import __version__  #pip 안하기 위해 주석 처리
 #####
+
+
+class logSumCoefficient(nn.Module):
+    def __init__(self,n):
+        super(logSumCoefficient, self).__init__()
+        self.n=n
+        self.model = torch.jit.load("m"+n+".pt")
+
+    def __init__(self,path):
+        super(logSumCoefficient, self).__init__()
+        self.model = torch.jit.load(path)
+
+    def predict(self,X_value):
+        pred=self.model(X_value)
+        log_individual = torch.sum(torch.log(X_value) * pred[:-1])
+        log_constant = pred[-1]
+        return log_individual+log_constant
+
+    def approximate(self,X_list):
+        print("approximiation start")
+        print(X_list)
+        l = X_list.size(dim=0)
+        ret = list(l)
+        for t in range(l):
+            ts=X_list[t]
+            ret.append(self.predict(ts))
+        return torch.tensor(ret)
+
+
 
 # constants
 
